00859448b983f10f3b5f4214323bd9a41ac82bf4
Ishan <ishan102201@gmail.com>
2024-06-16 01:37:55 -0400

    preview overlays

--- src/ButtonBox.js
+++ src/ButtonBox.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const Popup = ({ closePopup }) => (
+  <div className="popup-overlay visible">
+    <div className="popup-content">
+      <h2>Popup Title</h2>
+      <p>This is the content of the popup.</p>
+      <button onClick={closePopup}>Close</button>
+    </div>
+  </div>
+);
+
 const ButtonBox = ({ children }) => {
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const togglePopup = () => {
+    setIsPopupVisible(!isPopupVisible);
+  };
+
   return (
     <div className="box">
-      <button className="internalButton">Button</button>
+      <button className="internalButton" onClick={togglePopup}>Button</button>
       <div className="content">
         <div style={{ marginTop: '30px', marginBottom: '30px' }}>
           {children}
         </div>
       </div>
+      {isPopupVisible && <Popup closePopup={togglePopup} />}
     </div>
   );
 };
--- src/TabSlidePage.js
+++ src/TabSlidePage.js
@@ -1,40 +1,27 @@
+import React from 'react';
 import ButtonBox from './ButtonBox';
 import './App.css';
-import React, { useState, useEffect, useRef } from 'react';
-import TabUIElementTwo from './TabUIElementTwo'
+import TabUIElementTwo from './TabUIElementTwo';
 import TabUIElementOne from './TabUIElementOne';
-import AvatarCircles from './ThreeAvatar';
-import AvatarWithStatus from './AvatarWithStatus';
-import Checkboxes from './Checkboxes';
-import HoverMenu from './HoverMenu';
-import DropdownMenu from './HoverMenu';
-
 
 const TabSlidePage = () => {
-
-    
-
   return (
     <div>
-      
-    <div class="flex-container">
-        <div className='comp-card'><ButtonBox/>
-        <TabUIElementTwo/>
-       
+      <div className="flex-container">
+        <div className="comp-card">
+          <ButtonBox>
+            <TabUIElementTwo />
+          </ButtonBox>
         </div>
-
-  </div>
-
-<div class="flex-container">
-<div className='comp-card'><ButtonBox/>
-<TabUIElementOne/>
-
-      
-</div>
-</div>
-
-
-</div>
+      </div>
+      <div className="flex-container">
+        <div className="comp-card">
+          <ButtonBox>
+            <TabUIElementOne />
+          </ButtonBox>
+        </div>
+      </div>
+    </div>
   );
 };
 
